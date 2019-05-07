95174db64314a8a5e9296556d9b8389ea8adb569
erwinsnijder <48627520+erwinsnijder@users.noreply.github.com>
2019-05-07 11:13:31 +0200

    galgje
    random js array met woorden aangemaakt
    basis html opgemaakt

---
+++ script.js
@@ -0,0 +1,8 @@
+var randomwordarray = [
+    "Appels",
+    "Bananen",
+    "aardbeien",
+    "sinaasappelen"
+  ];
+  
+  var randomword = randomwordarray[Math.floor(Math.random()*randomwordArray.length)];
