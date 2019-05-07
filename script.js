d1f0759c921560905cc57211890080dbca1834c0
erwinsnijder <48627520+erwinsnijder@users.noreply.github.com>
2019-05-07 13:08:02 +0200

    werkend
    werkend

--- script.js
+++ script.js
@@ -1,9 +1,2 @@
-  var letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
-                'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    var wins = 0;
-    var losses = 0;
-    var guesses = 10;
-  
-    var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
-
-    console.log(computerChoice)
+function checkWord(){     if (document.getElementById("guess").value=="e")
+{     alert("Goedzo!");}     else {         alert("Bijna goed!")     } }
