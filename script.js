1984ab0d11e9fb8d26f6d33061bb0d9615d3ca48
erwinsnijder <48627520+erwinsnijder@users.noreply.github.com>
2019-05-07 11:55:44 +0200

    galgje
    versimpeld

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-var randomwordarray = [
-    "Appels",
-    "Bananen",
-    "aardbeien",
-    "sinaasappelen"
-  ];
+  var letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+                'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var wins = 0;
+    var losses = 0;
+    var guesses = 10;
   
-  var randomword = randomwordarray[Math.floor(Math.random()*randomwordArray.length)];
+    var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+    console.log(computerChoice)
